30b67ed0cb7d2e76d756b0356b8793ae5750005f
David Miguel Andrés <damigand700@gmail.com>
2024-12-19 01:52:42 +0100

    cositas
    terminado el responsive de todo lo que hay por el momento. Desactivado el poder navegar usando los botones del navegador para que el usuario no salga sin querer de la página, ya que la navegación es simulada

--- Frontend/pages/EventDetails/eventDetails.js
+++ Frontend/pages/EventDetails/eventDetails.js
@@ -1,14 +1,14 @@
-import makeRequest from '../../components/makeRequest/makeRequest.js';
-import backButton from '../../components/backButton/backButton.js';
-import userAvatar from '../../components/userAvatar/userAvatar.js';
-import Profile from '../Profile/Profile.js';
-import './eventDetails.css';
+import makeRequest from "../../components/makeRequest/makeRequest.js";
+import backButton from "../../components/backButton/backButton.js";
+import userAvatar from "../../components/userAvatar/userAvatar.js";
+import Profile from "../Profile/Profile.js";
+import "./eventDetails.css";
 
 const $ = (el) => document.querySelector(el);
 const $$ = (els) => document.querySelector(els);
 
 const template = () => {
-	return `
+    return `
         <div id="event-details">
             
             
@@ -17,43 +17,41 @@ const template = () => {
 };
 
 const getDetails = (event) => {
-	const eventDiv = document.createElement('div');
-	eventDiv.classList.add('event-info');
+    const eventDiv = document.createElement("div");
+    eventDiv.classList.add("event-info");
 
-	var attendants = document.createElement('div');
-	attendants.classList.add('attendants');
+    var attendants = document.createElement("div");
+    attendants.classList.add("attendants");
 
-	eventDiv.innerHTML = eventHTML(event);
-	attendants = attendantsHTML(attendants, event.asistentes, event._id);
+    eventDiv.innerHTML = eventHTML(event);
+    attendants = attendantsHTML(attendants, event.asistentes, event._id);
 
-	$('#event-details').appendChild(eventDiv);
-	$('#event-details').appendChild(attendants);
+    $("#event-details").appendChild(eventDiv);
+    $("#event-details").appendChild(attendants);
 
-	const creador = $('.creador-usuario');
-	creador.addEventListener('click', () => {
-		Profile(event.creador._id, { url: 'Event', id: event._id });
-	});
+    const creador = $(".creador-usuario");
+    creador.addEventListener("click", () => {
+        Profile(event.creador._id, { url: "Event", id: event._id });
+    });
 };
 
 const eventHTML = (event) => {
-	const options = {
-		weekday: 'long',
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric',
-	};
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
 
-	const date = new Date(event.fecha).toLocaleDateString('es-ES', options);
-	return `
+    const date = new Date(event.fecha).toLocaleDateString("es-ES", options);
+    return `
         <h1>${event.titulo}</h1>
         <div class="creador">Creado por <span class="creador-usuario">${
-				event.creador.usuario
-			}</span></div>
+            event.creador.usuario
+        }</span></div>
         <p>${
-				event.descripcion
-					? event.descripcion
-					: 'Este evento no tiene ninguna descripción.'
-			}</p>
+            event.descripcion ? event.descripcion : "Este evento no tiene ninguna descripción."
+        }</p>
         <div class="date">
             <i class="bx bxs-calendar"></i>
             <span>${date}</span>
@@ -66,78 +64,78 @@ const eventHTML = (event) => {
 };
 
 const attendantsHTML = (attendantsDiv, attendants, eventId) => {
-	const avatarsDiv = document.createElement('div');
-	avatarsDiv.classList.add('users');
+    const avatarsDiv = document.createElement("div");
+    avatarsDiv.classList.add("users");
 
-	//Ponemos un maximo de 5 asistentes
-	//que se verán en el div pequeño.
-	const max = 5;
+    //Ponemos un maximo de 5 asistentes
+    //que se verán en el div pequeño.
+    const max = 5;
 
-	//Por cada asistente creamos su div.
-	for (const user of attendants) {
-		const div = document.createElement('div');
-		div.classList.add('attendant');
-		div.innerHTML = userAvatar(true, user);
+    //Por cada asistente creamos su div.
+    for (const user of attendants) {
+        const div = document.createElement("div");
+        div.classList.add("attendant");
+        div.innerHTML = userAvatar(true, user);
 
-		const usuario = document.createElement('span');
-		usuario.innerText = user.usuario;
+        const usuario = document.createElement("span");
+        usuario.innerText = user.usuario;
 
-		const backButton = { url: 'Event', id: eventId };
-		usuario.addEventListener('click', () => Profile(user._id, backButton));
+        const backButton = { url: "Event", id: eventId };
+        usuario.addEventListener("click", () => Profile(user._id, backButton));
 
-		div.appendChild(usuario);
-		avatarsDiv.appendChild(div);
-	}
+        div.appendChild(usuario);
+        avatarsDiv.appendChild(div);
+    }
 
-	//Botón de "ver más" para ver todos los participantes
-	//Mediante CSS se verán solo 5 participantes hasta
-	//que se presione este botón y se muestren todos.
-	const seeMoreDiv = document.createElement('div');
-	seeMoreDiv.classList.add('more');
+    //Botón de "ver más" para ver todos los participantes
+    //Mediante CSS se verán solo 5 participantes hasta
+    //que se presione este botón y se muestren todos.
+    const seeMoreDiv = document.createElement("div");
+    seeMoreDiv.classList.add("more");
 
-	const more = attendants.length - max;
+    const more = attendants.length - max;
 
-	const span = document.createElement('span');
-	const text = `${more} más...`;
-	span.textContent = more > 0 ? text : 'Ver detalles';
+    const span = document.createElement("span");
+    const text = `${more} más...`;
+    span.textContent = more > 0 ? text : "Ver detalles";
 
-	seeMoreDiv.appendChild(span);
+    seeMoreDiv.appendChild(span);
 
-	//Alternamos aspectos visuales al dar click en el botón
-	//De ver participantes o ver menos.
-	seeMoreDiv.addEventListener('click', () => {
-		attendantsDiv.classList.toggle('active');
-		if (attendantsDiv.classList.contains('active')) {
-			span.textContent = 'Ver menos';
-		} else {
-			span.textContent = more > 0 ? text : 'Ver detalles';
-		}
+    //Alternamos aspectos visuales al dar click en el botón
+    //De ver participantes o ver menos.
+    seeMoreDiv.addEventListener("click", () => {
+        attendantsDiv.classList.toggle("active");
+        if (attendantsDiv.classList.contains("active")) {
+            span.textContent = "Cerrar";
+        } else {
+            span.textContent = more > 0 ? text : "Ver detalles";
+        }
 
-		//Animamos el aspecto del botón.
-		span.animate({ opacity: 0 }, { duration: 1000, direction: 'reverse' });
-	});
+        //Animamos el aspecto del botón.
+        span.animate({ opacity: 0 }, { duration: 1000, direction: "reverse" });
+    });
 
-	//Retornamos todo montado.
-	attendantsDiv.appendChild(avatarsDiv);
-	attendantsDiv.appendChild(seeMoreDiv);
-	return attendantsDiv;
+    //Retornamos todo montado.
+    attendantsDiv.appendChild(avatarsDiv);
+    attendantsDiv.appendChild(seeMoreDiv);
+    return attendantsDiv;
 };
 
 const eventDetails = async (eventId, backNav) => {
-	$('main').innerHTML = template();
-	const back = backButton(backNav);
-	$('#event-details').insertAdjacentElement('afterbegin', back);
+    $("main").innerHTML = template();
+    const back = backButton(backNav);
+    $("#event-details").insertAdjacentElement("afterbegin", back);
 
-	const url = `http://localhost:3000/api/events/${eventId}`;
-	const options = {
-		method: 'GET',
-	};
+    const url = `http://localhost:3000/api/events/${eventId}`;
+    const options = {
+        method: "GET",
+    };
 
-	const response = await makeRequest(url, options);
-	if (response.success) {
-		const event = response.json;
-		getDetails(event);
-	}
+    const response = await makeRequest(url, options);
+    if (response.success) {
+        const event = response.json;
+        getDetails(event);
+    }
 };
 
 export default eventDetails;
